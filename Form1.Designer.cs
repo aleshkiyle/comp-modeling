066f7caf2caac956fc6b5face4c53aae1864cf68
aleshkiyle <aleksey.rodin62@gmail.com>
2023-03-20 12:19:41 +0300

    add first lab

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(375, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            this.chart2.Location = new System.Drawing.Point(421, 12);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(348, 300);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Математическое ожидание";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Дисперсия";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 390);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Второй момент";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 423);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Третий момент";
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(577, 369);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(135, 34);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "Сгенерировать";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 469);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Обобщенный аддитивный генератор";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public int I = 9;
+        private int _Mm = 2015;
+        private double _Y = Math.Pow(2, 9);
+
+        /// <summary>
+        /// Количество случайных чисел, необходимых для 
+        /// получения нового случайного числа
+        /// </summary>
+        private const int R = 10;
+
+        /// <summary>
+        /// Таблица случаных чисел
+        /// </summary>
+        private double[] _Rand = new double[R] {0.87, 0.33, 0.29, 0.12, 0.95,
+        0.66, 0.19, 0.61, 0.27, 0.88} ;
+
+        /// <summary>
+        /// Объём выборки
+        /// </summary>
+        int V = 6000;
+
+        /// <summary>
+        /// Число участков разбиения
+        /// </summary>
+        int N = 16;
+
+        /// <summary>
+        /// Мат ожидание
+        /// </summary>
+        double MX = 0;
+
+        /// <summary>
+        /// Дисперсия
+        /// </summary>
+        double DX = 0;
+
+        /// <summary>
+        /// Второй момент
+        /// </summary>
+        double m2 = 0;
+
+        /// <summary>
+        /// Третий момент
+        /// </summary>
+        double m3 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButtonClick(object sender, EventArgs e)
+        {
+            double[] x = CalcValues();
+            PrintDiagramm(x);
+            PrintStaticFunc(x);
+            MX = CalcMatOzh(x);
+            DX = CalcDisp(x,MX);
+            m2 = Moments(x, 2);
+            m3 = Moments(x, 3);
+            label1.Text = "Мат. ожидание = " + Convert.ToString(MX);
+            label2.Text = "Дисперсия = " + Convert.ToString(DX);
+            label3.Text = "Второй момент = " + Convert.ToString(m2);
+            label4.Text = "Третий момент = " + Convert.ToString(m3);
+        }
+
+        public double CalcMatOzh(double[] x)
+        {
+            double M = 0;
+            for (int i = 0;i < x.Length; i++)
+            {
+                M += x[i];
+            }
+            return M/x.Length;
+        }
+
+        public double CalcDisp(double[] x, double Mx)
+        {
+            double D = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                D += x[i] * x[i];
+            }
+            D /= x.Length;
+            return (D - Mx * Mx) * x.Length / (x.Length - 1);
+        }
+
+        public double Moments(double[] x, int degree)
+        {
+            double sum = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                sum += Math.Pow(x[i], degree);
+            }
+            return sum / x.Length;
+        }
+
+        public double[] CalcValues()
+        {
+            double[] x = new double[V];
+            for (int i = 0; i < V; i++)
+            {
+                x[i] = Rnd();
+            }
+            return x;
+        }
+
+        public double Rnd()
+        {
+            double s = 0.0;
+            for (int k = 0; k < R; k ++)
+            {
+                s += _Rand[k]; // сумма случаных чисел
+            }
+            for (int k = 1; k < R; k ++)
+            {
+                // сдвиг случаных чисел
+                _Rand[k - 1] = _Rand[k]; 
+            }
+            s -= Math.Truncate(s);
+            _Rand[R - 1] = s;
+            return s;
+        }
+
+        public int CountLessValueFromRnds(double[] x, double value)
+        {
+            int count = 0;
+
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                if (x[i] < value)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Вывод гистограммы частот
+        /// </summary>
+        /// <param name="x"></param>
+        public void PrintDiagramm(double[] x)
+        {
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 1;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+
+            double[] xData = new double[V];
+            double[] yData = new double[V];
+
+            for (int i = 0; i < N; i++)
+            {
+                double count = 0;
+                for (int j = 0; j < x.Length; j++)
+                {
+                    if (x[j] >= i * ((double)1 / N) && x[j] < (i + 1) * ((double)1 / N))
+                    {
+                        count += 1;
+                    }
+                }
+
+                xData[i] = i * ((double)1 / N);
+                yData[i] = count / (V * ((double)1 / N));
+            }
+
+            chart1.Series[0].Points.DataBindXY(xData, yData);
+            chart1.Series[0].Name = "Гистограмма частот";
+
+        }
+
+        /// <summary>
+        /// Вывод статической функции
+        /// </summary>
+        /// <param name="x"></param>
+        public void PrintStaticFunc(double[] x)
+        {
+            chart2.ChartAreas[0].AxisX.Minimum = 0;
+            chart2.ChartAreas[0].AxisX.Maximum = 1;
+            chart2.ChartAreas[0].AxisY.Minimum = 0;
+            chart2.ChartAreas[0].AxisY.Maximum = 1;
+            int count = 1000;
+
+            double[] xs = new double[1000];
+            double[] ys = new double[1000];
+            double s = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                double y = (double)CountLessValueFromRnds(x, s) / V;
+
+                xs[i] = s;
+                ys[i] = y;
+                s += 0.001;
+            }
+
+            chart2.Series[0].Points.DataBindXY(xs, ys);
+            chart2.Series[0].Name = "Статическая функция";
+        }
+    }
+}
